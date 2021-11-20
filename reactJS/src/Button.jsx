47e8eace31310196a68ddef1ee081cbe11058c26
marcosDeveloperr <marcos.ti.profissional@gmail.com>
2021-11-20 20:59:42 -0300

    elemento Button

---
+++ reactJS/src/Button.jsx
@@ -0,0 +1,11 @@
+import React from "react";//preciso dele em todos os arquivos.
+
+function Button()
+{
+    //agora temos jsx
+    return(
+        <button>{}</button>
+    )
+}
+
+export default Button
