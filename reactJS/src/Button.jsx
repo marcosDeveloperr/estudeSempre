b58fb0599ad028c292dcc46ceda95b091ca3439f
marcosDeveloperr <marcos.ti.profissional@gmail.com>
2021-11-28 11:13:47 -0300

    atualizando projeto

--- reactJS/src/Button.jsx
+++ reactJS/src/Button.jsx
@@ -1,13 +1,13 @@
-import React from "react";//preciso dele em todos os arquivos.
+import React from "react";   //preciso dele em todos os arquivos.
 
 function Button(props)
 {
-    const {name, onClick} = props
-    
+    const {name, onClick} = props;
+
     //agora temos jsx
     return(
         <button onClick={onClick}>{name}</button>
     )
 }
 
-export default Button
+export default Button   
