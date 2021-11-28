450128c33a9b87b7ef3a72c05715b274b3e46812
marcosDeveloperr <marcos.ti.profissional@gmail.com>
2021-11-28 11:15:59 -0300

    javaEstudos

---
+++ java/javaEstudos/src/App.java
@@ -0,0 +1,16 @@
+//import fundamentos.Variaveis;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Oi, Mundo Jav!");     
+      
+        //fundamentos.Variaveis a = new fundamentos.Variaveis();
+        //Variaveis a = new Variaveis(); //importacao feita.
+        //System.out.println(a);
+
+        int idade = 13;
+        System.out.println(idade);
+
+    }
+       
+}
