70e39da9b8ef7208a5476af692a6a7742eef23f0
marcosDeveloperr <marcos.ti.profissional@gmail.com>
2021-12-02 12:21:53 -0300

    anotacoes - teoria

--- java/javaEstudos/src/fundamentos/Variaveis.java
+++ java/javaEstudos/src/fundamentos/Variaveis.java
@@ -1,3 +1,16 @@
+/**Teoria
+ *  
+ *  - Local de memória para armazenar valores. Tipos: Parâmetro, local, classe e instância de objeto.
+ * 
+ *  - Não começar com números. Não pode conter espaço, palavra reservada de java
+ * 
+ * *****Criação*****
+ * 
+ *  - public        static      tipo    nome    =   valor
+ *    protected     final
+ *    private
+ */
+
 public class Variaveis {
     
 }
