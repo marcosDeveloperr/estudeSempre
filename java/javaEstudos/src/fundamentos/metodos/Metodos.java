40c98f342d7ec4dfd07b55f4705cd45aa05933ae
marcosDeveloperr <marcos.ti.profissional@gmail.com>
2021-12-16 09:55:01 -0300

    exemplo correto met passagem parametor copia

--- java/javaEstudos/src/fundamentos/metodos/Metodos.java
+++ java/javaEstudos/src/fundamentos/metodos/Metodos.java
@@ -27,10 +27,14 @@ public class Metodos
     double bonus = 1.0d;
 
     //metodo por passagem por parametro - valor (copia)
-    public void bonusNota(double nota)
+    //dentro do metodo o valor do bonus é atualizado, mas colocando o System.out.println 
+    //fora do metodo continua tendo o valor 1.0d
+    //Isso é passagem por parametro por valor(copia)
+    
+    public void bonusNota(double bonus)
         {
-            nota = nota + bonus;
-            System.out.println("nota final com bonus 1.0: " + nota);
+            bonus = bonus + 5.0d;
+            System.out.println("Dentro do metodo o valor do bonus: " + bonus);
         }
    
     public static void main(String[] args)
@@ -38,6 +42,7 @@ public class Metodos
        
         Metodos met = new Metodos();
         met.bonusNota(6.0);
+      
         
     }    
     
