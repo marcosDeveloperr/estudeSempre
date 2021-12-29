66b5be45903f63bc3ffe93cde20d996738012bea
marcosDeveloperr <marcos.ti.profissional@gmail.com>
2021-12-29 15:07:22 -0300

    atualizando 1-string.js

--- javaScript/tipos/string/1-string.js
+++ javaScript/tipos/string/1-string.js
@@ -12,3 +12,9 @@ console.log('\nSubstituindo texto pelo outro: ', replaceText);
 //retorna a fatia de um valor
 const lastChar = 'texto'.slice(-1); //ultima letra de uma string
 console.log('\nUltima letra de uma string ', lastChar);
+
+const allWithoutLastChar = 'texto'.slice(0,-1);
+console.log('\nValor da string da primeira letra menos a ultima: ', allWithoutLastChar);
+
+const secondText =  'texto'.slice(1);
+console.log('\nValor da string da segunda letra até a última: ', secondText);
