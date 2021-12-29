61bc0b41f3e5f00e94c1679b56be29489be38de7
marcosDeveloperr <marcos.ti.profissional@gmail.com>
2021-12-29 15:15:24 -0300

    add number

---
+++ javaScript/tipos/number/1-number.js
@@ -0,0 +1,7 @@
+const myNumber = 12.4032;
+
+//transformar um numero em string
+const numberAsString = myNumber.toString();
+console.log('\nNumero transformado em string: ',numberAsString);
+console.log('tipo: ', typeof numberAsString);
+
