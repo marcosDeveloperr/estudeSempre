1ac79b7e3ac5f9d0b5b98647011ed4cb4efa637f
Marcos Developerr <marcos.ti.profissional@gmail.com>
2022-09-06 10:24:37 -0300

    package fundamentos_primeiro programa

---
+++ treinandoJava/src/fundamentos/PrimeiroPrograma.java
@@ -0,0 +1,10 @@
+package fundamentos;
+
+public class PrimeiroPrograma 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("time do meu coração: flamengo");//imprima na saida do programa
+	}
+
+}
