f14099e832919ea205648ce4c60c6d7edf52b68a
Marcos Developerr <marcos.ti.profissional@gmail.com>
2022-11-08 14:57:10 -0300

    add ExemploFor.java

---
+++ treinandoJava/src/dio/controle/fluxo/condicional/simples/ExemploFor.java
@@ -0,0 +1,15 @@
+package dio.controle.fluxo.condicional.simples;
+
+public class ExemploFor
+{
+
+	public static void main(String[] args)
+	{
+		//contando carneirinhos
+		for(int carneirinhos = 1; carneirinhos<=20; carneirinhos++)
+		{
+			System.out.println("Contanto Carneirinhos: " + carneirinhos);
+		}
+	}
+
+}
