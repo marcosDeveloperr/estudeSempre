96be23168671a18ec949b51967d1b98e513f3771
Marcos Developerr <marcos.ti.profissional@gmail.com>
2022-11-10 08:59:12 -0300

    add ExcecoesAboutMe.java

---
+++ treinandoJava/src/dio/controle/fluxo/condicional/simples/ExcecoesAboutMe.java
@@ -0,0 +1,47 @@
+package dio.controle.fluxo.condicional.simples;
+
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ExcecoesAboutMe 
+{
+	//about me "sobre mim"
+	//Exceções: 
+		//Não informar o nome e sobrenome;
+		//o valor da idade ter um caractere  não númerico;
+		//o valor da altura ter uma vírgula ao invés de ter um ponto
+	
+	public static void main(String[] args) 
+	{
+		//entrada:  nome, sobrenome, idade e altura
+		//saída: exibir os dados de entrada.
+		
+		//criando o objeto scanner chamado entrada
+		 
+		 
+					
+			
+			Scanner entrada = new Scanner(System.in).useLocale(Locale.US);
+			
+			//as entradas
+			System.out.println("Nome:");
+			String nome = entrada.next();
+			
+			System.out.println("Sobrenome:");
+			String sobrenome = entrada.next();
+			
+			System.out.println("Idade");
+			int idade = entrada.nextInt();
+			
+			System.out.println("Altura");
+			double altura = entrada.nextDouble();
+			
+			//imprimindo as saídas dos dados digitados pelo usuário
+			System.out.println("Nome: " + nome.toLowerCase() + " " + sobrenome.toLowerCase());
+			System.out.println("Idade: " + idade + " anos ");
+			System.out.println("Altura: " + altura + " cm ");
+		 
+		 
+	}
+}
