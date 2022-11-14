9d69a13e73a1f6216c98864013925a5f3655af8e
Marcos Developerr <marcos.ti.profissional@gmail.com>
2022-11-14 11:49:03 -0300

    add do while

--- treinandoJava/src/dio/controle/fluxo/condicional/simples/UncheckedException.java
+++ treinandoJava/src/dio/controle/fluxo/condicional/simples/UncheckedException.java
@@ -11,29 +11,35 @@ public class UncheckedException
 		//realizar a divisão de dois números inteiros.
 		//Saida do resultado da divisão.
 		
-		String numerador = JOptionPane.showInputDialog("Numerador");//colocando nome resulta uncheckedException
-		String denominador = JOptionPane.showInputDialog("Denominador");
+		boolean continuaLoopin = true;
+		do 
+		{
+			String numerador = JOptionPane.showInputDialog("Numerador");//colocando nome resulta uncheckedException
+			String denominador = JOptionPane.showInputDialog("Denominador");
+			
+			try 
+			{
+				int resultado = dividir(Integer.parseInt(numerador),Integer.parseInt(denominador));
+				System.out.println("resultado da divisão: " + resultado);
+				continuaLoopin = false;
+			} 
+			catch (NumberFormatException e) 
+			{
+				//e.printStackTrace();//imprimir
+				//quando acontecer exception - NumberFormatException - digitou um texto no lugar inteiro
+				//informe uma mensagem pelo motivo do erro ( entrada que realizou o erro)
+				JOptionPane.showMessageDialog(null, "Informe um numero inteiro: " + e.getMessage());
+			}
+			catch (ArithmeticException e) 
+			{
+				JOptionPane.showMessageDialog(null, "Denominador não poder ser zero ( 0 )");
+			}
+			finally 
+			{
+				System.out.println("chegou no finally");
+			}
+		} while (continuaLoopin);		
 		
-		try 
-		{
-			int resultado = dividir(Integer.parseInt(numerador),Integer.parseInt(denominador));
-			System.out.println("resultado da divisão: " + resultado);
-		} 
-		catch (NumberFormatException e) 
-		{
-			//e.printStackTrace();//imprimir
-			//quando acontecer exception - NumberFormatException - digitou um texto no lugar inteiro
-			//informe uma mensagem pelo motivo do erro ( entrada que realizou o erro)
-			JOptionPane.showMessageDialog(null, "Informe um numero inteiro: " + e.getMessage());
-		}
-		catch (ArithmeticException e) 
-		{
-			JOptionPane.showMessageDialog(null, "Denominador não poder ser zero ( 0 )");
-		}
-		finally 
-		{
-			System.out.println("chegou no finally");
-		}
 		System.out.println("o código continua...");
 		
 		
