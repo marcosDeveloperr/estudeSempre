892d1462aec0906275bcb3a784d5416ba72dd5af
Marcos Developerr <marcos.ti.profissional@gmail.com>
2022-11-15 11:22:20 -0300

    add Carro.java

---
+++ treinandoJava/src/dio/oo/estruturaBasica/Carro.java
@@ -0,0 +1,12 @@
+package dio.oo.estruturaBasica;
+
+//Criar uma classe carro
+//Criar os atributos: cor, modelo e capacidade do tanque
+
+public class Carro //classe criada 
+{
+	String cor;
+	String modelo;
+	int capacidadeTanque;  //atributos criados
+
+}
