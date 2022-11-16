539e73719cc4cee75912f43928af2e72a550550c
Marcos Developerr <marcos.ti.profissional@gmail.com>
2022-11-16 11:02:40 -0300

    add comentarios...

--- treinandoJava/src/dio/oo/estruturaBasica/RodarAplicacao.java
+++ treinandoJava/src/dio/oo/estruturaBasica/RodarAplicacao.java
@@ -12,18 +12,19 @@ public class RodarAplicacao
 		 * */
 		
 		//criando objetos - carro
-		Carro carro1 = new Carro(); 
-		Carro carro2 = new Carro("cinza","Mercedes-Benz Class C",66);
+		Carro carro1 = new Carro(); //construtor mais simples
+		Carro carro2 = new Carro("cinza","Mercedes-Benz Class C",66); //utilizando a sobrecarga
 		
+		//adicionando os valores nos atributos
 		carro1.setCor("Azul");
 		carro1.setModelo("BMW Serie 3");
-		carro1.setCapacidadeTanque(59);
+		carro1.setCapacidadeTanque(59); 
 		
 		
 		System.out.println("carro 1 - Cor: " + carro1.getCor());
 		System.out.println("carro 1 - Modelo: " + carro1.getModelo());
 		System.out.println("carro 1 - Capacidade do Tanque: " + carro1.getCapacidadeTanque());
-		System.out.println("carro 1 - Total valor para encher o tanque: " + carro1.totalValorTanque(5.00) + "\n");
+		System.out.println("carro 1 - Total valor para encher o tanque: " + carro1.totalValorTanque(5.00) + "\n"); //quebra de linha
 		
 		System.out.println("Carro 2 - Cor: " + carro2.getCor());
 		System.out.println("Carro 2 - Modelo: " + carro2.getModelo());
