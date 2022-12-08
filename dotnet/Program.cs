b3faeec0ce0ba03a6a5aeb2de18ab8527c12b696
Marcos Developerr <marcos.ti.profissional@gmail.com>
2022-12-08 18:00:48 -0300

    atualizando...

--- dotnet/Program.cs
+++ dotnet/Program.cs
@@ -1,3 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Testando o projeto console...");
+//Console.WriteLine("Hello, World!");
+//Console.WriteLine("Testando o projeto console...");
+
+using dotnet.Models;
+
+string apresentacao = "O time maior do Brasil - Flamengo";
+int quantidade = 10;
+double altura = 1.85;
+decimal preco = 7.50M; //A letra M tratando 2 casas decimais como foi adicionada o valor 
+bool condicao = true;
+
+Console.WriteLine(apresentacao);
+Console.WriteLine(quantidade);
+Console.WriteLine(altura);
+Console.WriteLine(preco);
+Console.WriteLine(condicao);
+
+
+//Pessoa p1 = new Pessoa();
+//p1.Nome = "Marcos";
+//p1.Idade = 24;
+//p1.Apresentar();
