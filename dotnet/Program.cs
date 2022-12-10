a878abf7b5e7dee736313761e829e02db17ebd1e
marcosDeveloperr <marcos.ti.profissional@gmail.com>
2022-12-10 11:58:51 -0300

    DataTime,Operador de Atribuicao

--- dotnet/Program.cs
+++ dotnet/Program.cs
@@ -3,18 +3,35 @@
 //Console.WriteLine("Testando o projeto console...");
 
 using dotnet.Models;
+//Operador de Atribuição
+int a = 10;
+int b = 20;
+int c = a + b;
+Console.WriteLine("A soma: " + c);
 
-string apresentacao = "O time maior do Brasil - Flamengo";
-int quantidade = 10;
-double altura = 1.85;
-decimal preco = 7.50M; //A letra M tratando 2 casas decimais como foi adicionada o valor 
-bool condicao = true;
+c = c + 5; //colocando mais 5 no valor de c 
+//c += 5; //também poderia ser assim, somando mais 5 no valor de c
+Console.WriteLine("Valor atual c :" + c);
 
-Console.WriteLine(apresentacao);
-Console.WriteLine(quantidade);
-Console.WriteLine(altura);
-Console.WriteLine(preco);
-Console.WriteLine(condicao);
+
+// DateTime dataAtual = DateTime.Now; //pegando a data atual do sistema
+// Console.WriteLine(dataAtual);
+
+// DateTime dataAtualAdicionada = DateTime.Now.AddDays(5);
+// Console.WriteLine(dataAtualAdicionada); //adicionando 5 dias a data atual do sistema
+// Console.WriteLine(dataAtual.ToString("dd/MM/yyyy")); //formata de data , sem horário.
+
+// string apresentacao = "O time maior do Brasil - Flamengo";
+// int quantidade = 10;
+// double altura = 1.85;
+// decimal preco = 7.50M; //A letra M tratando 2 casas decimais como foi adicionada o valor 
+// bool condicao = true;
+
+// Console.WriteLine(apresentacao);
+// Console.WriteLine(quantidade);
+// Console.WriteLine(altura);
+// Console.WriteLine(preco);
+// Console.WriteLine(condicao);
 
 
 //Pessoa p1 = new Pessoa();
