26a0e7d644fa3617247267f77eb0b70f7256890e
marcosDeveloperr <marcos.ti.profissional@gmail.com>
2022-12-12 10:29:52 -0300

    foreach

--- dotnet/dotnet/Program.cs
+++ dotnet/dotnet/Program.cs
@@ -17,6 +17,11 @@ for(int contador = 0; contador<numeros.Length; contador++)
     Console.WriteLine($"Posição {contador} - {numeros[contador]}");
 }
 
+foreach(int valor in numeros)
+{
+    Console.WriteLine(valor);
+}
+
 // Pessoa p10 = new Pessoa(); //classe pertecendo a outro projeto ExemploFundamentos.Common pertecendo a solution Pottencial .Net.sln
 // p10.Idade = 30;
 // p10.Apresentar();
