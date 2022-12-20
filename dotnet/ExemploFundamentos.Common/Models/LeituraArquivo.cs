dba6026217a1b2c288f93e5d2cdfb5027d0338ae
marcosDeveloperr <marcos.ti.profissional@gmail.com>
2022-12-20 08:14:47 -0300

    add LeituraArquivo.cs met LetArquivoTupla

---
+++ dotnet/ExemploFundamentos.Common/Models/LeituraArquivo.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExemploFundamentos.Common.Models
+{
+    public class LeituraArquivo
+    {
+        //metodo para ler um arquivo usando Tupla
+        public (bool sucesso, string[]linhas, int quantidadeLinha)LerAquivoTupla(string caminhoArquivo) 
+        {
+            try
+            {
+                //abrindo, lendo o arquivo e retornando uma matriz de string contendo todas as linhas do arquivo
+                string[] linhasRetornadas = File.ReadAllLines("arquivos/arquivoLeitura.txt");
+                return (true, linhasRetornadas, linhasRetornadas.Count());
+                
+            }
+            catch(Exception)
+            {
+
+                return (false,new string[0],0);
+            }
+        }
+    }
+}
