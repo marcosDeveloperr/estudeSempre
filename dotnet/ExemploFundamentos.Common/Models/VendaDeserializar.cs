d1f1fd1251a69d9e5991a10886e6902ed0ca39b6
marcosDeveloperr <marcos.ti.profissional@gmail.com>
2022-12-21 19:43:20 -0300

    utilizando ATRIBUTO

--- dotnet/ExemploFundamentos.Common/Models/VendaDeserializar.cs
+++ dotnet/ExemploFundamentos.Common/Models/VendaDeserializar.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace ExemploFundamentos.Common.Models
 {
@@ -11,6 +12,12 @@ namespace ExemploFundamentos.Common.Models
         //criando os atributos referente ao arquivo venda.json na pasta arquivos,
         //tendo os mesmo nomes.
         public int Id { get; set; }
+        
+        //variavel recibida do venda.json escrita Nome_Produto - estaria saindo da conversao do C# - evitar bagunçar o código
+        //Entra entra o ATRIBUTO   [JsonProperty("Nome_Produto")] 
+        //public string Nome_Produto { get; set; }
+       
+        [JsonProperty("Nome_Produto")] //estaria escrito em venda.json
         public string Produto { get; set; }
         public decimal Preco { get; set; }
         public DateTime DataVenda { get; set; }
