ce6474d5e9b0f016c116d40c1ccb9c093e1f356f
marcosDeveloperr <marcos.ti.profissional@gmail.com>
2022-12-21 11:53:08 -0300

    add Venda.cs

---
+++ dotnet/ExemploFundamentos.Common/Models/Venda.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace ExemploFundamentos.Common.Models
+{
+    //O cliente pediu os dados das vendas convertido em json
+    public class Venda
+    {
+         //Método Construtor
+        public Venda(int id, string produto, decimal preco)
+        {
+            this.Id = id;
+            this.Produto = produto;
+            this.Preco = preco;
+        }
+
+        //Propriedades (atributos)
+        public int Id { get; set; }
+        public string Produto { get; set; }
+        public decimal Preco { get; set; }
+
+       
+
+    }
+    
+}
