ad4ce4d636d1ab894cc3f5a03a50da0bc83404ea
marcosDeveloperr <marcos.ti.profissional@gmail.com>
2022-12-22 12:54:50 -0300

    add prop Desconto com ?

--- dotnet/ExemploFundamentos.Common/Models/Venda.cs
+++ dotnet/ExemploFundamentos.Common/Models/Venda.cs
@@ -10,17 +10,26 @@ namespace ExemploFundamentos.Common.Models
     public class Venda
     {
          //Método Construtor
-        public Venda(int id, string produto, decimal preco)
+        public Venda(int id, string produto, decimal preco, DateTime dataVenda)
         {
             this.Id = id;
             this.Produto = produto;
             this.Preco = preco;
+            this.DataVenda = dataVenda;
         }
 
         //Propriedades (atributos)
         public int Id { get; set; }
         public string Produto { get; set; }
         public decimal Preco { get; set; }
+        public DateTime DataVenda { get; set; }
+        
+        //Aceita valor null
+        //Colocar a propriedade ( ? ) logo depois do tipo decimal
+        //Não pode ser decimal, porque não aceita null
+        //adicionada no arquivo venda.json na pasta arquivos/
+        //public decimal Desconto { get; set; }
+        public decimal? Desconto { get; set; }
 
        
 
