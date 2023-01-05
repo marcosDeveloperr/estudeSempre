a037a5cabe93f698a0c0dbf8b89e6d07ebd38512
Marcos Developerr <marcos.ti.profissional@gmail.com>
2023-01-05 14:26:50 -0300

    override metodo Apresentar()

---
+++ dotnet/ExemploFundamentos.Common/Models/ExemploPOO/Professor.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExemploFundamentos.Common.Models.ExemploPOO
+{
+    public class Professor : Pessoa
+    {
+        public decimal Salario { get; set; }
+        
+        //override - sobrescrever
+        public override void Apresentar()
+        {
+            Console.WriteLine($"Olá, meu nome é {Nome}, tenho {Idade} anos, sou um professor e ganho {Salario}");
+        }
+    }
+}
