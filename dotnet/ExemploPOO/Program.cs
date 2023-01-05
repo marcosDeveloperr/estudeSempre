f4b855fce7135a6a1a434490a071d91ea2b03fa2
Marcos Developerr <marcos.ti.profissional@gmail.com>
2023-01-05 10:53:34 -0300

    utilizando met ExibirSaldo,Sacar

--- dotnet/ExemploPOO/Program.cs
+++ dotnet/ExemploPOO/Program.cs
@@ -4,7 +4,47 @@
 
 using ExemploFundamentos.Common.Models.ExemploPOO;
 
-Pessoa p1 = new Pessoa();
-p1.Nome = "Leonardo";
-p1.Idade = 20;
-p1.Apresentar();
+ContaCorrente c1 = new ContaCorrente(123, 1000);
+//c1.saldo - erro porque saldo é privado
+c1.ExibirSaldo();
+c1.Sacar(500);
+c1.ExibirSaldo();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Pessoa p1 = new Pessoa();
+// p1.Nome = "Leonardo";
+// p1.Idade = 20;
+// p1.Apresentar();
