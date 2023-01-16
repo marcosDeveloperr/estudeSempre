6a4296967f3cc3f0434c515c96e75cb91821f39d
marcosDeveloperr <marcos.ti.profissional@gmail.com>
2023-01-16 11:18:04 -0300

    crud configurado

--- dotnet/ProjetoMVC/Context/AgendaContext.cs
+++ dotnet/ProjetoMVC/Context/AgendaContext.cs
@@ -2,15 +2,20 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjetoMVC.Models;
 
 namespace ProjetoMVC.Context
 {
     public class AgendaContext : DbContext
     {
-        public AgendaContext(DbContextOptions<AgendaContext> options) : base(options)
+        //construtor
+        public AgendaContext(DbContextOptions<AgendaContext>options): base(options)
         {
-
+           
         }
-        public DbSet<ContextMarshalException> Contato {get; set;}
+        
+        //Criando a tabela Contato 
+        public DbSet<Contato> Contato {get; set;}
     }
 }
--- dotnet/ProjetoMVC/Models/Contato.cs
+++ dotnet/ProjetoMVC/Models/Contato.cs
@@ -2,11 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProjetoMVC.Models
 {
+   
     public class Contato
     {
+        
         public int Id { get; set; }
         public string  Nome { get; set; }
         public string Telefone { get; set; }
